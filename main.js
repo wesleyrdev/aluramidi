3459bf60729ec7e92a22146c0649b319096c5ef5
Wesley R. Sales <93395344+wesleyrdev@users.noreply.github.com>
2023-04-04 14:39:11 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+const listaDeTeclas = document.querySelectorAll ('.tecla');
 
+let contador = 0;
+
+while (contador < listaDeTeclas.length) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = listaDeTeclas[contador].classList[1];
+    const idAudio =`#som_${instrumento}`;
+    
+    console.log(instrumento);
+
+    
+    
+    tecla.onclick = function () {
+        tocaSom('#som_tecla_pom');
+    }
+
+    contador = contador + 1;
+
+    console.log(contador);
+} 
 

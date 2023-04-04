4f1d04dc215c1bde85cd246346c7b5e7719b4b1a
Wesley R. Sales <93395344+wesleyrdev@users.noreply.github.com>
2023-04-04 15:11:28 -0300

    v1

--- main.js
+++ main.js
@@ -12,12 +12,10 @@ while (contador < listaDeTeclas.length) {
     const instrumento = listaDeTeclas[contador].classList[1];
     const idAudio =`#som_${instrumento}`;
     
-    console.log(instrumento);
+    //console.log(idAudio);
 
-    
-    
     tecla.onclick = function () {
-        tocaSom('#som_tecla_pom');
+        tocaSom();
     }
 
     contador = contador + 1;
